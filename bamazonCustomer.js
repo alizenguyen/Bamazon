bfff3251d9b667ac65f682fd991b65e7a4ff956c
Alize Nguyen <alizenguyen@gmail.com>
2017-12-08 17:02:12 -0600

    displayproduct function added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,19 +4,34 @@ var inquirer = require('inquirer');
 
 // establish connection
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "MyNewPass",
-  database: "bamazon"
+    // Your password
+    password: "MyNewPass",
+    database: "bamazon"
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    displayProducts();
+    connection.end();
 });
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("-----------------------------------");
+            for (var i = 0; i < res.length; i++) {
+                console.log(res[i].item_id + " : " + res[i].product_name + ", " + res[i].price);
+            }
+            console.log("-----------------------------------");
+        }
+    });
+};
