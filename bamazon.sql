f81d80bb747891da4684ef9390ba26de1f816ea9
Alize Nguyen <alizenguyen@gmail.com>
2017-12-08 16:53:39 -0600

    packages installed and applied to js file

--- bamazon.sql
+++ bamazon.sql
@@ -7,8 +7,8 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
-  price INTEGER(45) NULL,
-  stock_quantity VARCHAR(45) NULL,  
+  price DECIMAL(12,2) NULL,
+  stock_quantity INT(45) NULL,  
   PRIMARY KEY (item_id)
 );
 
