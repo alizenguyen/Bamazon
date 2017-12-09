88f9e31ee898a2f41bcb284f6a57ee033c9e04a1
Alize Nguyen <alizenguyen@gmail.com>
2017-12-08 19:51:37 -0600

    add inventory function added

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,18 +26,17 @@ function managerDuties() {
         type: "list",
         message: "Hello boss. Please select what you would like to do today?",
         name: "duty",
-        choices: ["view products for sale", "view low inventory", "add to invetory", "add new product"]
+        choices: ["view products for sale", "view low inventory", "add to inventory", "add new product"]
     }, ]).then(function (answer) {
         if (answer.duty === "view products for sale") {
             viewProducts();
         } else if (answer.duty === "view low inventory") {
             lowInventory();
         } else if (answer.duty === "add to inventory") {
-            console.log("add invetory function here")
+            addInventory();
         } else {
             console.log("add new product here")
         };
-        connection.end();
     });
 };
 
@@ -51,6 +50,7 @@ function viewProducts() {
                 console.log(res[i].item_id + " : " + res[i].product_name + ", Price " + res[i].price + ", Quantity " + res[i].stock_quantity);
             }
             console.log("-----------------------------------");
+            connection.end();
         }
     });
 };
@@ -62,11 +62,71 @@ function lowInventory() {
         var lowInvent = [];
         for (var i = 0; i < res.length; i += 1) {
             if (res[i].stock_quantity < 5) {
-                lowInvent.push("\nID: " + res[i].item_id + " ITEM: " + res[i].product_name + " QUANTITY: " + res[i].stock_quantity );
+                lowInvent.push("\nID: " + res[i].item_id + " ITEM: " + res[i].product_name + " QUANTITY: " + res[i].stock_quantity);
             }
         };
         console.log("---------------------------------------------------------------------------------------------------");
         console.log("Low inventory (stock quantity below 5):" + lowInvent);
         console.log("---------------------------------------------------------------------------------------------------");
+        connection.end();
     })
+};
+
+function addInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+
+        inquirer.prompt([{
+            type: "list",
+            message: "Hello boss! What would you like to restock now?",
+            name: "restock",
+            choices: function() {
+                var choiceArray = [];
+                for(var i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].product_name);
+                }
+                return choiceArray;
+                },
+            },    
+            {
+                type: "input",
+                message: "How much would you like to add to your inventory?",
+                name: "numberrestock"
+            }
+        ]).then(function (answer) {
+            var restockProduct = answer.restock;
+
+            var restockQuantity;
+            for (var i = 0; i < res.length; i += 1) {
+                if (res[i].product_name === answer.restock) {
+                    restockQuantity = res[i].stock_quantity;
+                }
+            };
+            
+            var restockTotal = parseInt(answer.numberrestock) + restockQuantity;
+
+            connection.query(
+                "UPDATE products SET ? WHERE ?", [
+                    {
+                        stock_quantity: restockTotal
+                    },
+                    {
+                        product_name: restockProduct
+                    }
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log("---------------------------------------------------------------------------------------------------");
+                        console.log("Your inventory has been updated!")
+                        console.log("You now have updated " + restockProduct + " to a quantity of " + restockTotal + "." )
+                        console.log("---------------------------------------------------------------------------------------------------");
+                        connection.end();
+                    }
+                }
+            )
+        });
+    });
 };
