bb42c15cb872eb2ba63a289c637ba0005893d9aa
Alize Nguyen <alizenguyen@gmail.com>
2017-12-08 18:51:02 -0600

    promp function for bamazon manager added

---
+++ bamazonManager.js
@@ -0,0 +1,44 @@
+// allows users to create and bid on assorted items, tasks, jobs, or projects
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+
+// establish connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "MyNewPass",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    managerDuties();
+});
+
+function managerDuties() {
+    inquirer.prompt([
+    {
+        type: "list",
+        message: "Hello boss. Please select what you would like to do today?",
+        name: "duty",
+        choices: ["view products for sale", "view low inventory", "add to invetory", "add new product"]
+    },
+    ]).then(function (answer) {
+        if (answer.duty === "view products for sale") {
+            console.log("view product function here");
+        } else if (answer.duty === "view low inventory") {
+            console.log("low inventory function here");
+        } else if (answer.duty === "add to inventory") {
+            console.log("add invetory function here")
+        } else {
+            console.log("add new product here")
+        };
+        connection.end();
+    });
+};
